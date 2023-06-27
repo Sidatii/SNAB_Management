83f24a4616bfb94f06214d0a3dfdd32781aee32c
abdallah <abddoullah.bouzhar@gmail.com>
2023-06-27 23:59:27 +0100

    nh version

--- force-app/main/default/lwc/displayArticles/displayArticles.js
+++ force-app/main/default/lwc/displayArticles/displayArticles.js
@@ -7,5 +7,14 @@ export default class DisplayArticles extends LightningElement {
     handleClick(){
         console.log(this.wiredArticles);
     }
+    // filter data based on search
+    handleSearch(event){
+        console.log(event.target.value);
+        const searchKey = event.target.value.toLowerCase();
+        const articles = this.wiredArticles.data;
+        const filteredArticles = articles.filter(article => {
+            return article.article.Title.toLowerCase().includes(searchKey);
+        });
+        this.wiredArticles.data = filteredArticles;
+    }
 }
-
