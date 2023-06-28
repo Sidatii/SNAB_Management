c3a07fe5bdf171496dc2da8009f71449600b4911
abdallah <abddoullah.bouzhar@gmail.com>
2023-06-28 11:52:53 +0100

    last modifications

--- force-app/main/default/lwc/displayArticles/displayArticles.js
+++ force-app/main/default/lwc/displayArticles/displayArticles.js
@@ -1,20 +1,44 @@
-import { LightningElement, wire} from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import getArticles from '@salesforce/apex/getArticlesController.getArticles';
 
 export default class DisplayArticles extends LightningElement {
 
     @wire (getArticles) wiredArticles;
     handleClick(){
-        console.log(this.wiredArticles);
+        // redirect to the article page
+        console.log('clicked');
+
     }
     // filter data based on search
-    handleSearch(event){
-        console.log(event.target.value);
-        const searchKey = event.target.value.toLowerCase();
-        const articles = this.wiredArticles.data;
-        const filteredArticles = articles.filter(article => {
-            return article.article.Title.toLowerCase().includes(searchKey);
-        });
-        this.wiredArticles.data = filteredArticles;
+    searchKey;
+    // @track means if any change in searchKey then it will reflect in wiredArticles
+    @track wiredArticles;
+    //This Funcation will get the value from Text Input.
+    handelSearchKey(event){
+        this.searchKey = event.target.value;
+        getArticles(this.searchKey)
     }
+
+    //This funcation will fetch the Account Name on basis of searchkey
+    SearchArticlesHandler(){
+        //call Apex method.
+        getArticles({textkey: this.searchKey})
+        .then(result => {
+            this.wiredArticles = result;
+
+        })
+        .catch( error=>{
+            console.log('Error Occured',error);
+        });
+
+    }
+    // handleSearch(event){
+    //     console.log(event.target.value);
+    //     const searchKey = event.target.value.toLowerCase();
+    //     const articles = this.wiredArticles.data;
+    //     const filteredArticles = articles.filter(article => {
+    //         return article.article.Title.toLowerCase().includes(searchKey);
+    //     });
+    //     this.wiredArticles.data = filteredArticles;
+    // }
 }
